73c5822ac05abab2c8f71062bf5b36676e529b33
Wontory <tjddnjsjo@kakao.com>
2023-04-27 21:48:11 +0900

    chore: add axios dependency

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 
+import axios from "axios";
+
 const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/test/test/");
-      const result = res.json();
-      return result;
-    };
-
-    fetchData().then((res) => setData(res));
+    axios
+      .get("/api/test/test/")
+      .then((res) => setData(res))
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return <div className="App">{JSON.stringify(data)}</div>;
--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/test/test/", {
+    createProxyMiddleware("/api", {
       target: "https://9klnwopt8e.apigw.ntruss.com",
       changeOrigin: true,
     })
