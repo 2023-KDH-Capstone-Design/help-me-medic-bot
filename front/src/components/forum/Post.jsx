4561334522afc856e6d600ceaefd8b5bf37796a6
Wontory <tjddnjsjo@kakao.com>
2023-04-29 00:44:22 +0900

    refactor: Change bootstrap to tailwind

--- front/src/components/forum/Post.jsx
+++ front/src/components/forum/Post.jsx
@@ -1,33 +1,24 @@
 import React from "react";
 
 const Post = () => {
+  const handleSubmit = () => {
+    alert("test");
+  };
+
   return (
-    <div
-      class="border border-success p-2 mb-2 border-opacity-50 rounded"
-      style={{ margin: "0.5em" }}
-    >
-      <div class="d-grid gap-2" style={{ margin: "0.5em" }}>
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Add post"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            style={{ height: "10em" }}
-          />
-        </div>
-        <div class="d-grid gap-2">
-          <button
-            type="button"
-            class="btn btn-primary"
-            style={{ height: "3em" }}
-          >
+    <form className="card w-full bg-base-100 shadow-xl" onSubmit={handleSubmit}>
+      <div className="card-body">
+        <textarea
+          className="textarea textarea-bordered"
+          placeholder="What's on your mind?"
+        />
+        <div className="card-actions justify-end">
+          <button type="submit" className="btn btn-primary">
             Post
           </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
