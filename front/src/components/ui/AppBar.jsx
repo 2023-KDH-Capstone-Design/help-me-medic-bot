302678a278ab643dc772bc25fb6c5586d9848748
Wontory <tjddnjsjo@kakao.com>
2023-04-28 18:03:18 +0900

    feat: render hamburger button only on mobile view

--- front/src/components/ui/AppBar.jsx
+++ front/src/components/ui/AppBar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 import { FaBars } from "react-icons/fa";
 
 const AppBar = (props) => {
+  const matches = useMediaQuery({ query: "(max-width:1023px)" });
+
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-none">
-        <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
-          <FaBars />
-        </label>
-      </div>
+      {matches && (
+        <div className="flex-none">
+          <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
+            <FaBars />
+          </label>
+        </div>
+      )}
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">{props.title}</a>
       </div>
