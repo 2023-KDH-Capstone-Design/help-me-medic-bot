4faa81f91ae89cd0129eab9ea83130eec8cd79ba
Wontory <tjddnjsjo@kakao.com>
2023-04-28 17:55:06 +0900

    style: Rearrange navbar

--- front/src/components/ui/AppBar.jsx
+++ front/src/components/ui/AppBar.jsx
@@ -5,13 +5,13 @@ import { FaBars } from "react-icons/fa";
 const AppBar = (props) => {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">{props.title}</a>
-      </div>
       <div className="flex-none">
         <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
           <FaBars />
         </label>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">{props.title}</a>
       </div>
     </div>
   );
