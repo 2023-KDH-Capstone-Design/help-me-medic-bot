c1abccedacd94113f065839bca610f7bf9e2e93c
Wontory <tjddnjsjo@kakao.com>
2023-04-29 22:51:38 +0900

    fix: Change class prop to className

--- front/src/components/forum/Post.jsx
+++ front/src/components/forum/Post.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 const Post = () => {
   return (
-    <div class="border-solid border-2 border-grey rounded w-full">
-      <div class="m-3">
-        <img src="dog.jpg" alt="" class="w-8 h-8 inline m-2 rounded" />
-        <p class="text-xl inline">
+    <div className="border-solid border-2 border-grey rounded w-full">
+      <div className="m-3">
+        <img src="dog.jpg" alt="" className="w-8 h-8 inline m-2 rounded" />
+        <p className="text-xl inline">
           <b>JoMeongWon</b>
         </p>
       </div>
-      <div class="mb-3 ml-8 text-xl">Where we go?</div>
+      <div className="mb-3 ml-8 text-xl">Where we go?</div>
       <div>
-        <form class="m-2">
-          <label class="inline">
+        <form className="m-2">
+          <label className="inline">
             <input
               type="text"
               placeholder="Comment!"
-              class="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
                     focus:outline-none focus:border-sky-500"
             />
           </label>
-          <div class="card-actions justify-end">
-            <button type="submit" class="btn btn-primary rounded-xl w-20">
+          <div className="card-actions justify-end">
+            <button type="submit" className="btn btn-primary rounded-xl w-20">
               SEND
             </button>
           </div>
