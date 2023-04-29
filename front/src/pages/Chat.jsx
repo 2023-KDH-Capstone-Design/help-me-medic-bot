393964a0ff46395bc9985b1a42c46f1681e2e8b6
Wontory <tjddnjsjo@kakao.com>
2023-04-30 01:27:33 +0900

    refactor: Erase api test code

--- front/src/pages/Chat.jsx
+++ front/src/pages/Chat.jsx
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import ChatForm from "../components/chat/ChatForm";
 
 const Chat = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("/api/test/test/")
-      .then((res) => setData(res))
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <>
       <ChatForm />
-      <div>{JSON.stringify(data)}</div>
     </>
   );
 };
