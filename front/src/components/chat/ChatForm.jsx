0cfa84580f398a14d202b19931132f93930c1c85
Wontory <tjddnjsjo@kakao.com>
2023-04-30 01:27:43 +0900

    refactor: Edit style

--- front/src/components/chat/ChatForm.jsx
+++ front/src/components/chat/ChatForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatForm = () => {
   return (
-    <section class="msger max-w-3xl mx-auto bg-gray-100">
+    <section class="msger w-full mx-auto flex flex-col gap-8 mt-8">
       <main class="msger-chat">
         <div class="chat chat-start ml-2">
           <div class="chat-image avatar">
@@ -10,12 +10,7 @@ const ChatForm = () => {
               <img src="kimdamae.jpg" alt="face" />
             </div>
           </div>
-          <div class="chat-header">
-            kim da mae
-            <time class="text-xs opacity-50">12:45</time>
-          </div>
           <div class="chat-bubble">나는 개입니다. 월월</div>
-          <div class="chat-footer opacity-50">Delivered</div>
         </div>
         <div class="chat chat-end mr-2">
           <div class="chat-image avatar">
@@ -23,23 +18,116 @@ const ChatForm = () => {
               <img src="dog.jpg" alt="face" />
             </div>
           </div>
-          <div class="chat-header">
-            jo boong sin
-            <time class="text-xs opacity-50">12:46</time>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
           </div>
           <div class="chat-bubble">저는 조붕딱입미다.</div>
-          <div class="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
+        </div>
+        <div class="chat chat-start ml-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="kimdamae.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">나는 개입니다. 월월</div>
+        </div>
+        <div class="chat chat-end mr-2">
+          <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+              <img src="dog.jpg" alt="face" />
+            </div>
+          </div>
+          <div class="chat-bubble">저는 조붕딱입미다.</div>
         </div>
       </main>
-      <form class="msger-inputarea">
-        <input
-          type="text"
-          placeholder="Type here"
-          class="input input-bordered w-full max-w-[84%]"
-        />
-        <button type="submit" class="btn btn-outline btn-info">
-          Send
-        </button>
+      <form class="flex-0 msger-inputarea">
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+            <button className="btn btn-outline btn-info">Send</button>
+          </div>
+        </div>
       </form>
     </section>
   );
