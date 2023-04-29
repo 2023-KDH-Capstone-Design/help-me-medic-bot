b2272e452070ac6e29d2157d0b6726f047124918
Wontory <tjddnjsjo@kakao.com>
2023-04-29 22:54:39 +0900

    refactor: Change media query to tailwind

--- front/src/components/ui/AppBar.jsx
+++ front/src/components/ui/AppBar.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
 
 import { FaBars } from "react-icons/fa";
 
 const AppBar = (props) => {
-  const matches = useMediaQuery({ query: "(max-width:1023px)" });
-
   return (
     <div className="navbar bg-base-100">
-      {matches && (
-        <div className="flex-none">
-          <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
-            <FaBars />
-          </label>
-        </div>
-      )}
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">{props.title}</a>
+      <div className="flex-none lg:hidden">
+        <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
+          <FaBars />
+        </label>
       </div>
+      <div className="flex-1 px-2 mx-2">{props.title}</div>
     </div>
   );
 };
