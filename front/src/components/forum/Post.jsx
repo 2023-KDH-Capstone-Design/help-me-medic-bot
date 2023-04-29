4fd8a23e2100b19616a4e86378587d5ec0d54f02
Wontory <tjddnjsjo@kakao.com>
2023-04-30 00:32:59 +0900

    refactor: Change tailwind to daisyui

--- front/src/components/forum/Post.jsx
+++ front/src/components/forum/Post.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 
+import { BsSend } from "react-icons/bs";
+
 const Post = () => {
   return (
-    <div className="border-solid border-2 border-grey rounded w-full">
-      <div className="m-3">
-        <img src="dog.jpg" alt="" className="w-8 h-8 inline m-2 rounded" />
-        <p className="text-xl inline">
-          <b>JoMeongWon</b>
-        </p>
-      </div>
-      <div className="mb-3 ml-8 text-xl">Where we go?</div>
-      <div>
-        <form className="m-2">
-          <label className="inline">
+    <div className="card w-full bg-base-100 shadow-xl">
+      <div className="card-body">
+        <div className="flex items-center space-x-2">
+          <div className="avatar">
+            <div className="w-8 rounded">
+              <img src="dog.jpg" alt="" />
+            </div>
+          </div>
+          <p>Name</p>
+        </div>
+        <p className="my-2 text-lg">어디로 가야하오</p>
+        <div className="form-control">
+          <div className="input-group input-group-md">
             <input
               type="text"
-              placeholder="Comment!"
-              className="mt-1 w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
-                    focus:outline-none focus:border-sky-500"
+              placeholder="Comment"
+              className="input input-bordered input-md w-full"
             />
-          </label>
-          <div className="card-actions justify-end">
-            <button type="submit" className="btn btn-primary rounded-xl w-20">
-              SEND
+            <button className="btn btn-square btn-md">
+              <BsSend size="16px" />
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
