6be76ee9c31579a16352419eb129b49459e2f741
Wontory <tjddnjsjo@kakao.com>
2023-04-30 01:36:05 +0900

    style: *

--- front/src/components/chat/ChatForm.jsx
+++ front/src/components/chat/ChatForm.jsx
@@ -4,14 +4,6 @@ const ChatForm = () => {
   return (
     <section class="msger w-full mx-auto flex flex-col gap-8 mt-8">
       <main class="msger-chat">
-        <div class="chat chat-start ml-2">
-          <div class="chat-image avatar">
-            <div class="w-10 rounded-full">
-              <img src="kimdamae.jpg" alt="face" />
-            </div>
-          </div>
-          <div class="chat-bubble">나는 개입니다. 월월</div>
-        </div>
         <div class="chat chat-end mr-2">
           <div class="chat-image avatar">
             <div class="w-10 rounded-full">
@@ -107,14 +99,6 @@ const ChatForm = () => {
             </div>
           </div>
           <div class="chat-bubble">나는 개입니다. 월월</div>
-        </div>
-        <div class="chat chat-end mr-2">
-          <div class="chat-image avatar">
-            <div class="w-10 rounded-full">
-              <img src="dog.jpg" alt="face" />
-            </div>
-          </div>
-          <div class="chat-bubble">저는 조붕딱입미다.</div>
         </div>
       </main>
       <form class="flex-0 msger-inputarea">
