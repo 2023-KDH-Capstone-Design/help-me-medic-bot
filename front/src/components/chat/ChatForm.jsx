151282e1610da0f771e9a3984472271840e6c966
Wontory <tjddnjsjo@kakao.com>
2023-05-02 11:07:30 +0900

    feat: Add background at chat form

--- front/src/components/chat/ChatForm.jsx
+++ front/src/components/chat/ChatForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatForm = () => {
   return (
-    <form className="sticky bottom-0 p-4 msger-inputarea bg-base-300">
+    <form className="sticky bottom-0 p-4 msger-inputarea bg-gradient-to-t from-base-300 via-base-300">
       <div className="form-control">
         <div className="input-group">
           <input
