cb1f9234dcc4c797ffbc37c46b65a64a6541544e
Wontory <tjddnjsjo@kakao.com>
2023-05-02 11:27:15 +0900

    feat: Set appbar title

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, Routes, Route } from "react-router-dom";
 
 import AppBar from "./components/ui/AppBar";
 import Home from "./pages/Home";
@@ -8,21 +8,35 @@ import Forum from "./pages/Forum";
 import Advertisement from "./components/ui/Advertisement";
 import Drawer from "./components/ui/Drawer";
 
+const pages = [
+  { id: 0, path: "/", title: "Home", comp: <Home /> },
+  { id: 1, path: "/chat", title: "Chat", comp: <Chat /> },
+  { id: 2, path: "/forum", title: "Forum", comp: <Forum /> },
+];
+
 const App = () => {
+  const { pathname } = useLocation();
+  const [title, setTitle] = useState();
+
+  useEffect(() => {
+    const { title: subTitle } = pages.find((p) => p.path === pathname);
+    setTitle(subTitle);
+  }, [pathname]);
+
   return (
     <div className="bg-base-300 drawer drawer-mobile">
       <input type="checkbox" id="drawer" className="drawer-toggle" />
       <div className="drawer-content">
         <div className="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content shadow-sm">
-          <AppBar title="title" />
+          <AppBar title={title} />
         </div>
         <div className="px-6 xl:pr-2 mt-6">
           <div className="flex flex-col-reverse justify-between gap-6 xl:flex-row">
             <div className="w-full max-w-4xl flex-grow">
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/forum" element={<Forum />} />
+                {pages.map((p) => (
+                  <Route key={p.id} path={p.path} element={p.comp} />
+                ))}
               </Routes>
             </div>
             <div>
