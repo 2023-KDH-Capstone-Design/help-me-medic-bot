fdd052eaf45aef14a3b4da625ad801767845588d
Wontory <tjddnjsjo@kakao.com>
2023-05-03 15:02:00 +0900

    feat: Add language button

---
+++ front/src/components/ui/LanguageButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { FaGlobe } from "react-icons/fa";
+
+const LanguageButton = () => {
+  return (
+    <button className="btn btn-square btn-ghost w-12 h-10 rounded-lg">
+      <FaGlobe size="20px" />
+    </button>
+  );
+};
+
+export default LanguageButton;
