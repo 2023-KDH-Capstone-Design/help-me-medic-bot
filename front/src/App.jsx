fb1739ffcd781348e564c6aff93596bf13d1fd69
Wontory <tjddnjsjo@kakao.com>
2023-05-04 21:13:25 +0900

    refactor: Edit signup form & link

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import LogIn from "./pages/LogIn";
 import Main from "./pages/Main";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -10,7 +12,18 @@ const App = () => {
     if (sessionStorage.getItem("name") !== null) setIsLogin(true);
   }, []);
 
-  return isLogin ? <Main /> : <LogIn />;
+  return isLogin ? (
+    <Main />
+  ) : (
+    <div className="flex justify-center h-screen overflow-hidden">
+      <div className="w-[80%] p-6 space-y-4 m-auto bg-base-200 rounded-md shadow-md ring-2 ring-gray-800/50 md:max-w-md">
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -1,90 +1,91 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div class="relative flex flex-col justify-center h-screen overflow-hidden">
-      <div class="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
-        <h1 class="text-3xl font-semibold text-center text-gray-700">
-          <div className="text-primary inline-flex text-lg transition-all duration-200 md:text-3xl">
-            <span class="lowercase">Medi</span>
-            <span class="text-base-content uppercase">Chat</span>
-          </div>
-        </h1>
-        <form class="space-y-4">
-          <div>
-            <label class="label label-text">
-              <span class="text-base">ID</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter ID"
-              class="w-full input input-bordered"
-            />
-          </div>
-          <div>
-            <label class="label label-text">
-              <span class="text-base">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              class="w-full input input-bordered"
-            />
-          </div>
-          <div>
-            <label class="label label-text">
-              <span class="text-base">Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Name"
-              class="w-full input input-bordered"
-            />
-          </div>
-          <div>
-            <label class="label label-text">
-              <span class="text-base">Nickname</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Nickname"
-              class="w-full input input-bordered"
-            />
-          </div>
-          <div>
-            <label class="label">
-              <span class="text-base">Country</span>
-            </label>
-            <select class="select select-bordered w-full">
-              <option disabled selected>
-                Select Country
-              </option>
-              <option>South korea</option>
-              <option>USA</option>
-              <option>UK</option>
-              <option>spain</option>
-              <option>prance</option>
-              <option>france</option>
-              <option>Indonesia</option>
-              <option>Japan</option>
-              <option>China</option>
-              <option>Taiwan</option>
-              <option>Portugal</option>
-              <option>Russia</option>
-            </select>
-          </div>
-          <div>
-            <button class="btn btn-block">Sign Up</button>
-          </div>
-          <span>
-            Already have an account ?
-            <a href="#" class="text-base text-gray-500">
+    <>
+      <h1 className="text-primary text-xl font-semibold text-center transition-all duration-200 md:text-3xl">
+        <span>Create </span>
+        <span className="text-base-content">Account</span>
+      </h1>
+      <form className="space-y-4">
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="w-full input input-bordered"
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="w-full input input-bordered"
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full input input-bordered"
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Nickname</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Nickname"
+            className="w-full input input-bordered"
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Country</span>
+          </label>
+          <select
+            className="select select-bordered w-full"
+            defaultValue="Select Country"
+          >
+            <option disabled>Select Country</option>
+            <option>South korea</option>
+            <option>USA</option>
+            <option>UK</option>
+            <option>spain</option>
+            <option>prance</option>
+            <option>france</option>
+            <option>Indonesia</option>
+            <option>Japan</option>
+            <option>China</option>
+            <option>Taiwan</option>
+            <option>Portugal</option>
+            <option>Russia</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            Already have an account?
+            <Link
+              to="/"
+              className="text-gray-600 hover:underline hover:text-blue-600"
+            >
               Login
-            </a>
-          </span>
-        </form>
-      </div>
-    </div>
+            </Link>
+          </div>
+          <button className="btn btn-block">Sign Up</button>
+        </div>
+      </form>
+    </>
   );
 };
 
