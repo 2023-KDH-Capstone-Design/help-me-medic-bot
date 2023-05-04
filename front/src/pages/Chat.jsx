90f895abfa68dae1759abaa3a647defcaeb714bf
bbjbc <jobc4550@gmail.com>
2023-05-04 11:22:47 +0900

    refactor: Add modal component in chat component

--- front/src/pages/Chat.jsx
+++ front/src/pages/Chat.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import ChatLog from "../components/chat/ChatLog";
 import ChatForm from "../components/chat/ChatForm";
+import Modal from "../components/modal/Modal";
 
 const Chat = () => {
   return (
     <section className="msger w-full mx-auto flex flex-col gap-8">
       <ChatLog />
+      <div className="flex mx-3">
+        <Modal />
+      </div>
       <ChatForm />
     </section>
   );
