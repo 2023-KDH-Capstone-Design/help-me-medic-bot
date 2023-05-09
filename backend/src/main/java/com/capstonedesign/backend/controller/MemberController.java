951ad89e4a6fa9a86330c0a164343774616ab689
jae woo Jung <awfjol2008@naver.com>
2023-05-09 11:15:32 +0900

    feat: MemberController API 명세 추가

--- backend/src/main/java/com/capstonedesign/backend/controller/MemberController.java
+++ backend/src/main/java/com/capstonedesign/backend/controller/MemberController.java
@@ -5,6 +5,8 @@ import com.capstonedesign.backend.domain.member.service.MemberService;
 import com.capstonedesign.backend.domain.member.service.dto.request.JoinMemberRequestDTO;
 import com.capstonedesign.backend.domain.member.service.dto.request.UpdateMemberRequestDTO;
 import com.capstonedesign.backend.domain.member.service.dto.response.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "회원 관련 API")
 public class MemberController {
 
   private final MemberService memberService;
@@ -21,6 +24,7 @@ public class MemberController {
    * 회원 가입
    */
   @PostMapping("/member/join")
+  @ApiOperation(value = "회원가입 API", notes = "회원 정보를 요청값으로 받아 회원가입 진행")
   public JoinMemberResponseDTO joinMember(@RequestBody JoinMemberRequestDTO joinMemberRequestDTO) {
 
     Member member = new Member();
@@ -37,6 +41,7 @@ public class MemberController {
    * 회원 조회
    */
   @GetMapping("/member/list")
+  @ApiOperation(value = "전체 회원 목록 조회 API", notes = "회원가입된 모든 회원 목록 조회")
   public ListMemberResponse<List<DetailMemberResponseDTO>> listMember() {
 
     List<Member> memberList = memberService.findAll();
@@ -50,6 +55,7 @@ public class MemberController {
    * 회원 상세 조회
    */
   @GetMapping("/member/list/{memberId}")
+  @ApiOperation(value = "회원 상세 조회 API", notes = "특정 회원 상세 정보 조회")
   public DetailMemberResponseDTO listDetailMember(@PathVariable Long memberId) {
 
     Member findMember = memberService.findById(memberId);
@@ -60,7 +66,8 @@ public class MemberController {
   /**
    * 회원 수정
    */
-  @PostMapping("/member/update/{memberId}")
+  @PatchMapping("/member/update/{memberId}")
+  @ApiOperation(value = "회원 수정 API", notes = "요청받은 회원 정보 수정값에 따라 회원 수정")
   public UpdateMemberResponseDTO updateMember(@PathVariable Long memberId, @RequestBody UpdateMemberRequestDTO updateMemberRequestDTO) {
 
     memberService.updateMember(memberId, updateMemberRequestDTO);
@@ -73,6 +80,7 @@ public class MemberController {
    * 회원 삭제
    */
   @DeleteMapping("/member/delete/{memberId}")
+  @ApiOperation(value = "회원 삭제 API", notes = "특정 회원의 가입 정보를 삭제")
   public DeleteMemberResponseDTO deleteMember(@PathVariable Long memberId) {
 
     memberService.deleteById(memberId);
