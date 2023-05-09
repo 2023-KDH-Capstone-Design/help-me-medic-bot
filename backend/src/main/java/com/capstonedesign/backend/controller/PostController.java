2f61a7ac9432d10c164172637e146b21572aee2b
jae woo Jung <awfjol2008@naver.com>
2023-05-09 11:15:51 +0900

    feat: PostController API 명세 추가

--- backend/src/main/java/com/capstonedesign/backend/controller/PostController.java
+++ backend/src/main/java/com/capstonedesign/backend/controller/PostController.java
@@ -8,6 +8,8 @@ import com.capstonedesign.backend.domain.post.service.PostService;
 import com.capstonedesign.backend.domain.post.service.dto.request.CreatePostRequestDTO;
 import com.capstonedesign.backend.domain.post.service.dto.request.UpdatePostRequestDTO;
 import com.capstonedesign.backend.domain.post.service.dto.response.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "게시글 관련 API")
 public class PostController {
 
   private final PostService postService;
@@ -25,6 +28,7 @@ public class PostController {
    * 게시글 작성
    */
   @PostMapping("/post/create")
+  @ApiOperation(value = "게시글 작성 API", notes = "제목 및 내용을 요청 파라미터로 하여 게시글 작성")
   public CreatePostResponseDTO registerPost(@RequestBody CreatePostRequestDTO createPostRequestDTO) {
 
     Member findMember = memberService.findByName(createPostRequestDTO.getAuthor());
@@ -39,6 +43,7 @@ public class PostController {
    * 게시글 조회
    */
   @GetMapping("/post/list")
+  @ApiOperation(value = "게시글 목록 조회 API", notes = "전체 게시글 목록 조회")
   public ListPostResponse<List<DetailPostResponseDTO>> listPost() {
 
     List<Post> posts = postService.findAll();
@@ -54,6 +59,7 @@ public class PostController {
    * 게시글 상세 조회
    */
   @GetMapping("/post/list/{postId}")
+  @ApiOperation(value = "특정 게시글 상세 조회 API", notes = "특정 게시물 상세 조회")
   public DetailPostResponseDTO listPostDetail(@PathVariable Long postId) {
 
     Post findPost = postService.findById(postId);
@@ -65,7 +71,8 @@ public class PostController {
   /**
    * 게시글 수정
    */
-  @PostMapping("/post/update/{postId}")
+  @PatchMapping("/post/update/{postId}")
+  @ApiOperation(value = "게시글 수정 API", notes = "요청받은 게시글 정보 수정값에 따라 작성")
   public UpdatePostResponseDTO updatePost(@PathVariable Long postId, @RequestBody UpdatePostRequestDTO updatePostRequestDTO) {
 
     postService.updatePost(postId, updatePostRequestDTO);
@@ -79,6 +86,7 @@ public class PostController {
    * 게시글 삭제
    */
   @DeleteMapping("/post/delete/{postId}")
+  @ApiOperation(value = "게시글 삭제 API", notes = "특정 게시글 삭제")
   public DeletePostResponseDTO deletePost(@PathVariable Long postId) {
 
     postService.deleteById(postId);
