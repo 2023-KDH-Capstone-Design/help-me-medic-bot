be8191575c5f390771405c50b0d323f4af87f9fd
Wontory <tjddnjsjo@kakao.com>
2023-05-14 22:02:20 +0900

    refactor: 현재 위치 띄우기

--- frontend/src/components/modal/NaverMap.jsx
+++ frontend/src/components/modal/NaverMap.jsx
@@ -1,38 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const { naver } = window;
 
 const NaverMap = (props) => {
   const id = `map-${props.label.toLowerCase().replace(/\s+/g, "-")}`;
-  const [myLocation, setMyLocation] = useState({
-    latitude: 37.5112,
-    longitude: 127.0981,
-  });
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setMyLocation({ latitude, longitude });
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
+
+        const map = new naver.maps.Map(id, {
+          center: new naver.maps.LatLng(lat, lng),
+          zoom: 15,
+        });
+
+        const marker = new naver.maps.Marker({
+          position: new naver.maps.LatLng(lat, lng),
+          map: map,
+        });
       });
     } else {
       window.alert("현재위치를 알수 없습니다.");
     }
-  }, []);
-
-  const { latitude, longitude } = myLocation;
-
-  useEffect(() => {
-    const map = new naver.maps.Map(id, {
-      center: new naver.maps.LatLng(latitude, longitude),
-      zoom: 15,
-    });
-
-    const marker = new naver.maps.Marker({
-      position: new naver.maps.LatLng(latitude, longitude),
-      map: map,
-    });
-  }, [id, latitude, longitude]);
+  }, [id]);
 
   return <div id={id} style={{ width: "100%", height: "350px" }}></div>;
 };
