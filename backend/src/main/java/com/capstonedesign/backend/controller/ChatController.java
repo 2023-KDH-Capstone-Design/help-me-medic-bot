b2a5deb21c0a896869e77d256f1edcc6710f20ba
jae woo Jung <awfjol2008@naver.com>
2023-05-17 10:02:41 +0900

    feat: 클로바 챗봇 API 구현

--- backend/src/main/java/com/capstonedesign/backend/controller/ChatController.java
+++ backend/src/main/java/com/capstonedesign/backend/controller/ChatController.java
@@ -1,13 +1,18 @@
 package com.capstonedesign.backend.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.http.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,27 +22,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Controller
 public class ChatController {
 
-    //[시크릿키];
-    private String secretKey = "T1V3ekhsb21sdmZucmR5enhjTmVmRmZXSGhLSGNxU20=";
-    //[apiUrl키];
-    private String apiUrl = "https://31rdp4nzbo.apigw.ntruss.com/custom/v1/10315/d5386ba2920ffcf729e555bcfe9ef2cf78494ff05465f89bc301d1b1916bddce";
+    private static String secretKey = "S2hFQnJtYVhQUXVZeUZQeGFXUXBUY0pMY1RwcWprV08=";
+    private static String apiUrl = "https://9klnwopt8e.apigw.ntruss.com/custom/v1/10307/f0032dbc440d074bb96f85880cabc76b1b76b1cb1beee6e5ac977104e4c73457";
+
+    @GetMapping("chat")
+    public String chat() {
+        return "chat";
+    }
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
     public String sendMessage(@Payload String chatMessage) throws IOException {
 
-        URL url = new URL(apiUrl);
+        String translatedMessage1 = null;
+        try {
+            translatedMessage1 = extract(translate("en", "ko", chatMessage));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        String message = getReqMessage(chatMessage);
+        URL url = new URL(apiUrl);
+        String message =  getReqMessage(translatedMessage1);
         String encodeBase64String = makeSignature(message, secretKey);
 
         //api서버 접속 (서버 -> 서버 통신)
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "application/json;UTF-8");
         con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", encodeBase64String);
@@ -52,11 +68,11 @@ public class ChatController {
 
         BufferedReader br;
 
-        if (responseCode == 200) { // 정상 호출
+        if(responseCode==200) { // 정상 호출
 
             BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            con.getInputStream(), "UTF-8"));
+                new InputStreamReader(
+                    con.getInputStream(), "UTF-8"));
             String decodedString;
             String jsonString = "";
             while ((decodedString = in.readLine()) != null) {
@@ -67,11 +83,11 @@ public class ChatController {
             JSONParser jsonparser = new JSONParser();
             try {
                 JSONObject json = (JSONObject) jsonparser.parse(jsonString);
-                JSONArray bubblesArray = (JSONArray) json.get("bubbles");
-                JSONObject bubbles = (JSONObject) bubblesArray.get(0);
-                JSONObject data = (JSONObject) bubbles.get("data");
+                JSONArray bubblesArray = (JSONArray)json.get("bubbles");
+                JSONObject bubbles = (JSONObject)bubblesArray.get(0);
+                JSONObject data = (JSONObject)bubbles.get("data");
                 String description = "";
-                description = (String) data.get("description");
+                description = (String)data.get("description");
                 chatMessage = description;
             } catch (Exception e) {
                 System.out.println("error");
@@ -82,11 +98,18 @@ public class ChatController {
         } else {  // 에러 발생
             chatMessage = con.getResponseMessage();
         }
+
+        try {
+            chatMessage = extract(translate("ko", "en", chatMessage));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return chatMessage;
     }
 
     //보낼 메세지를 네이버에서 제공해준 암호화로 변경해주는 메소드
-    public String makeSignature(String message, String secretKey) {
+    public static String makeSignature(String message, String secretKey) {
 
         String encodeBase64String = "";
 
@@ -102,7 +125,7 @@ public class ChatController {
 
             return encodeBase64String;
 
-        } catch (Exception e) {
+        } catch (Exception e){
             System.out.println(e);
         }
 
@@ -111,7 +134,7 @@ public class ChatController {
     }
 
     //보낼 메세지를 네이버 챗봇에 포맷으로 변경해주는 메소드
-    public String getReqMessage(String voiceMessage) {
+    public static String getReqMessage(String voiceMessage) {
 
         String requestBody = "";
 
@@ -121,7 +144,7 @@ public class ChatController {
 
             long timestamp = new Date().getTime();
 
-            System.out.println("##" + timestamp);
+            System.out.println("##"+timestamp);
 
             obj.put("version", "v2");
             obj.put("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");
@@ -145,11 +168,52 @@ public class ChatController {
 
             requestBody = obj.toString();
 
-        } catch (Exception e) {
+        } catch (Exception e){
             System.out.println("## Exception : " + e);
         }
 
         return requestBody;
 
     }
+
+    // 번역
+    public String translate(String source, String target, String text) {
+
+        String clientId = "umeasvtipu"; // Client ID 및 Client Secret은 자신의 인증 정보를 발급 후 수정
+        String clientSecret = "3USaPOeHpUIF215ySb8NqAEk1VKzIvWxVZoibNFt";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("X-NCP-APIGW-API-KEY-ID", clientId);
+        headers.add("X-NCP-APIGW-API-KEY", clientSecret);
+
+        String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+        String body = "source=" + source + "&target=" + target + "&text=" + encodedText;
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+            "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation",
+            HttpMethod.POST,
+            requestEntity,
+            String.class
+        );
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return responseEntity.getBody();
+        } else {
+            return "Error occurred!";
+        }
+    }
+
+    // Json 응답으로부터 번역된 텍스트를 추출
+    public String extract(String message) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(message);
+        JsonNode translatedTextNode = rootNode.path("message").path("result").path("translatedText");
+
+        return translatedTextNode.asText();
+    }
+
 }
