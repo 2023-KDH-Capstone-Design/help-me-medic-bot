451e5f8518d389271f7d4708933ce90eea359262
Wontory <tjddnjsjo@kakao.com>
2023-05-18 10:16:09 +0900

    fix: 카카오맵 API 제거

--- frontend/src/components/modal/NaverMap.jsx
+++ frontend/src/components/modal/NaverMap.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const { naver, kakao } = window;
+const { naver } = window;
 
 const NaverMap = (props) => {
   const id = `map-${props.label.toLowerCase().replace(/\s+/g, "-")}`;
@@ -20,32 +20,6 @@ const NaverMap = (props) => {
           position: new naver.maps.LatLng(lat, lng),
           map: map,
         });
-
-        const kakaoService = new kakao.maps.services.Places();
-
-        const request = {
-          location: new kakao.maps.LatLng(lat, lng),
-          radius: 10000,
-          category_group_code: "HP8",
-        };
-
-        const keywords = "병원";
-
-        kakaoService.keywordSearch(
-          keywords,
-          (data, status) => {
-            if (status === kakao.maps.services.Status.OK) {
-              data.forEach((hospital) => {
-                const hospitalMarker = new naver.maps.Marker({
-                  position: new naver.maps.LatLng(hospital.y, hospital.x),
-                  map: map,
-                  title: hospital.place_name,
-                });
-              });
-            }
-          },
-          request
-        );
       });
     } else {
       window.alert("현재 위치를 알 수 없습니다.");
