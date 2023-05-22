43e4cdfcab6f633cbcdfc31010eb92f21edd82b9
jae woo Jung <awfjol2008@naver.com>
2023-05-22 23:14:42 +0900

    feat: 댓글 상세 조회 Response DTO 구현

---
+++ backend/src/main/java/com/capstonedesign/backend/domain/comment/service/dto/response/ListCommentDetailResponseDTO.java
@@ -0,0 +1,23 @@
+package com.capstonedesign.backend.domain.comment.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ListCommentDetailResponseDTO {
+
+  private Long id;
+
+  private Long postId;
+
+  private String author;
+
+  private String content;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+}
