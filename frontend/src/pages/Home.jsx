81ccf31c7280aee994ec58a2ffda32b1cd38e75b
bbjbc <jobc4550@gmail.com>
2023-05-22 13:26:30 +0900

    refactor: 반응형 수정 및 사진 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,22 +3,23 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 import { BsChatDots, BsPeople } from "react-icons/bs";
+import Image from "../assets/images/ai.jpg";
 
 const Home = () => {
   const { t } = useTranslation();
   return (
     <div>
       <div className="flex justify-center border-solid border-2 border-black rounded-lg h-56 xl:h-96 mb-5">
-        광고2
+        <img src={Image} alt="chatbot" className="w-full" />
       </div>
       <div className="grid md:grid-cols-2 gap-2 flex justify-center">
-        <div className="flex inline-flex btn btn-outline rounded-lg lg:h-32 h-24 lg:w-auto w-96">
+        <div className="flex inline-flex btn btn-outline rounded-lg lg:h-32 h-24 md:w-auto w-96">
           <Link to="/chat" className="flex gap-4">
             <BsChatDots size="40px" />
             <span className="text-4xl">{t("chat")}</span>
           </Link>
         </div>
-        <div className="flex inline-flex btn btn-outline rounded-lg lg:h-32 h-24 lg:w-auto w-96">
+        <div className="flex inline-flex btn btn-outline rounded-lg lg:h-32 h-24 md:w-auto w-96">
           <Link to="/forum" className="flex gap-4">
             <BsPeople size="40px" />
             <span className="text-4xl">{t("forum")}</span>
