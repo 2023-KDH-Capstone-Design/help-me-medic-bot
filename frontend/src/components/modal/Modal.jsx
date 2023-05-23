72005940f1b8da7a1ab440c50717ad697048d9ff
Wontory <tjddnjsjo@kakao.com>
2023-05-23 19:29:33 +0900

    test: Modal 수정

--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -18,8 +18,28 @@ const ModalOverlay = (props) => {
             ✕
           </label>
           <h3 className="text-lg font-bold">{label}</h3>
-          <p className="py-4">{`${label}에 관한 정보입니다.`}</p>
+          <p className="py-4">
+            세브란스병원은 서울특별시 서대문구 신촌동에 위치한 병원이다.
+            연세대학교 의료원 산하의 병원이며 3차 급여기관이자 대한민국의 상급
+            종합병원이다.
+            <br />
+            주소: 서울특별시 서대문구 신촌동 연세로 50-1
+            <br />
+            다음 장소에 위치: 연세대학교 신촌캠퍼스
+            <br />
+            연락처: 02-2228-1004
+            <br />
+            창립: 1885년
+            <br />
+            모회사: 연세의료원
+            <br />
+            영업시간: 영업 종료 ⋅ 수 오전 8:30에 영업 시작
+            <br />
+            종류: 3차 의료급여기관
+            <br />
+          </p>
           <NaverMap label={label} />
+          <button className="btn btn-info mt-4">병원 길안내</button>
         </label>
       </label>
     </>
