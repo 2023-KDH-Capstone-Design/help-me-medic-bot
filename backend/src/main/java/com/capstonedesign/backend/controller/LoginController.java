403b988705cb515b94ed904f0b896e0ff1bf60f2
Wontory <tjddnjsjo@kakao.com>
2023-05-24 23:09:13 +0900

    fix: React 서버 crossorigin 허용

--- backend/src/main/java/com/capstonedesign/backend/controller/LoginController.java
+++ backend/src/main/java/com/capstonedesign/backend/controller/LoginController.java
@@ -15,6 +15,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"})
 @Slf4j
 @Api(tags = "로그인 관련 API")
 public class LoginController {
